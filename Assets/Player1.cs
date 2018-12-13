e9bc836b97f903bae42fb6e98304fc309d0c80b5
MKruijer <45063316+MKruijer@users.noreply.github.com>
2018-12-13 10:58:12 +0100

    AlphaBetaGamma

--- Assets/Player1.cs
+++ Assets/Player1.cs
@@ -7,23 +7,31 @@ using System;
 public class Player1 : MonoBehaviour {
 
 	public static int DiceNumber = 0;
+	public static int EndTurn = 0;
 	public float PlayerPositionId = 0;
+	public float Geld = 200;
+	public static int test =5;
 
 	
 
 	public void DiceRoll()
 	{
-		PlayerPositionId += 1;
+		DiceNumber = UnityEngine.Random.Range(2,12);
+		print(DiceNumber);
+
 	}
 
-	void Update()
+	public void Bewegen()
 	{
+		//PlayerPositionId = PlayerPositionId + DiceNumber;
+
 		if(PlayerPositionId == 0) 
 		{
 			var pos = transform.position;
      		pos.x = 4.5f;
 			pos.z = -4.5f;
    			transform.position = pos;
+			GetMoneyz();
 		}
 		if(PlayerPositionId == 1) 
 		{
@@ -46,6 +54,19 @@ public class Player1 : MonoBehaviour {
 			pos.z = -4.5f;
    			transform.position = pos;
 		}
+	}
+
+	public void ActionButton()
+	{
+		if(PlayerPositionId == 1)
+		{
+			
+		}
+	}
+
+	public void GetMoneyz()
+	{
+		Geld = Geld + 200;
 	}
 
 }
