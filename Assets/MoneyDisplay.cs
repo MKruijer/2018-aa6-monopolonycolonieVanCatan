e589a333c8182a292736003a580a43f9908517bd
huiselijkgeweld <45063001+huiselijkgeweld@users.noreply.github.com>
2018-12-17 11:13:48 +0100

    Add files via upload

--- Assets/MoneyDisplay.cs
+++ Assets/MoneyDisplay.cs
@@ -1,14 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class MoneyDisplay : MonoBehaviour {
 
-	public Text GeldDIsplay;
+	public Text GeldDisplay;
 	
 	// Update is called once per frame
 	void Update () {
-		GeldDIsplay.text = "You have "+Player1.Geld+" roebels!";
-	}
+        if (CurrentPlayerScript.CurrentPlayer == 1)
+        {
+            GeldDisplay.text = "You have " + Player1Script.Player1Geld + " roebels!";
+        }
+        if (CurrentPlayerScript.CurrentPlayer == 2)
+        {
+            GeldDisplay.text = "You have " + Player2Script.Player2Geld + " roebels!";
+        }
+        if (CurrentPlayerScript.CurrentPlayer == 3)
+        {
+            //GeldDisplay.text = "You have " + Player3.Geld + " roebels!";
+        }
+        if (CurrentPlayerScript.CurrentPlayer == 4)
+        {
+           //GeldDisplay.text = "You have " + Player4.Geld + " roebels!";
+        }
+    }
 }
--- Assets/StraatKostScript.cs
+++ Assets/StraatKostScript.cs
@@ -7,14 +7,27 @@ public class StraatKostScript : MonoBehaviour {
 
 	public Text StraatDisplay;
 	public static int Straat1Kost = 50;
-	public static int Straat1Gekocht = 0;
-	
-	// Update is called once per frame
-	void Update () {
-		if(Straat1Gekocht == 0){
-		StraatDisplay.text = ""+Straat1Kost;
-		}
-		else {
+    public static int Straat2Kost = 100;
+    public static int Straat3Kost = 200;
+    public static int EigenaarStraat1 = 0;
+    public static int EigenaarStraat2 = 0;
+    public static int EigenaarStraat3 = 0;
+
+    void Update () {
+        if (BewegenScript.PlayerPositionRekenen == 1 && EigenaarStraat1 == 0)
+        {
+            StraatDisplay.text = "Deze straat kost: " + Straat1Kost + " om te kopen.";
+        }
+        else if (BewegenScript.PlayerPositionRekenen == 2 && EigenaarStraat2 == 0)
+        {
+		    StraatDisplay.text = "Deze straat kost: " + Straat2Kost + " om te kopen.";
+        }
+        else if (BewegenScript.PlayerPositionRekenen == 3 && EigenaarStraat3 == 0)
+        {
+            StraatDisplay.text = "Deze straat kost: " + Straat3Kost + " om te kopen.";
+        }
+        else
+        {
 			StraatDisplay.text = "";
 		}
 	}
